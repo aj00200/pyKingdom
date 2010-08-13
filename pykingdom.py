d6aeef6a1fa3214a15ab48bbea61367e2b570bf0
Austin H <aj00200@cube.(none)>
2010-08-13 09:58:32 +0200

    Added strings folder, started world1 strings file to describe villages. World1 now imports colorz

--- pykingdom.py
+++ pykingdom.py
@@ -1,14 +1,30 @@
 #! /usr/bin/python
-
+import sys
+sys.path.insert(1,sys.path[0]+'/strings')
 print('Choose your name:')
 name=raw_input('Name: ')
 print('')
-print('Choose your type:')
-print('1 - Sorcorer')
-print('2 - Warrior')
 
 print('Please enter type number:')
 try:
     type=input('Type: ')
 except:
     print('Please enter a number!')
+print('')
+print('Choose World: ')
+print('1 - Castalem')
+
+print('Please enter world number:')
+try:
+    world=input('World: ')
+except:
+    print('Please enter a number!')
+print('')
+if world==1:
+    import world1
+    world1.play(name,type)
+
+
+
+
+print('Thanks for playing!')
---
+++ strings/strings_1.py
@@ -0,0 +1,7 @@
+village_descriptions={
+#===Castalem Villages===
+'castalem':'Castalem is the Empire\'s capital, located on the Eastern side of the Forean Sea, which brings in most of the trade.',
+'tierth':'Other than Castalem, Tierth is the Empires  biggest trading village  which can be expected since it is located just north of the capital, also on the Forean Sea. It is also close enough to Casalem to get reinforcements if the need arises.',
+'Litopia': 'A small village located on the north-eastern part of the Forean Sea. It is a normal sized village, but because of its isolation, it does not recieve much trade. They have also grown lazy in their ways and the size of their army has been decreasing.',
+'Ceron':'A large military village on the Western Side of the Forean Sea which relies heavily on supplies from the rest of the Empire to keep its troops alive. They are the last Empire controlled village before the Parbean Empire.'
+}
